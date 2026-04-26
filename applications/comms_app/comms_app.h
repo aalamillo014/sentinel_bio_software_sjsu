a72a3dc452709c3d46939844753257e075b0c98e
aalamillo014 <159941023+aalamillo014@users.noreply.github.com>
2026-04-25 20:10:25 -0700

    Update comms_app.h

--- applications/comms_app/comms_app.h
+++ applications/comms_app/comms_app.h
@@ -1,10 +1,13 @@
 #ifndef COMMS_APP_H
 #define COMMS_APP_H
 
+#include "packet_format.h"
+
 void COMMS_Init(void);
 void COMMS_AppMain(void);
+void COMMS_Create_SciencePacket(void);
 void COMMS_Receive_Iridium(void);
-void COMMS_Send_SBand(const char *data);
+void COMMS_Send_SBand(const SciencePacket_t *packet);
 void COMMS_Send_Iridium(const char *data);
 
 #endif
