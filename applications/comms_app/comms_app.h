8be2756210543b0027b6e5f229aac34ee0aec429
aalamillo014 <159941023+aalamillo014@users.noreply.github.com>
2026-04-25 19:44:52 -0700

    Update comms_app.h

--- applications/comms_app/comms_app.h
+++ applications/comms_app/comms_app.h
@@ -2,7 +2,7 @@
 #define COMMS_APP_H
 
 void COMMS_Init(void);
-void COMMS_Run(void);
+void COMMS_AppMain(void);
 void COMMS_Receive_Iridium(void);
 void COMMS_Send_SBand(const char *data);
 void COMMS_Send_Iridium(const char *data);
